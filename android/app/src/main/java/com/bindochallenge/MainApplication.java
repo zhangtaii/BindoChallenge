ba866caacc6acd11217c90368a01f915cfd3a011
Tai Zhang <q6112345@gmail.com>
2017-09-16 12:23:05 +0800

    [ADD]react-native-barcodescanner

--- android/app/src/main/java/com/bindochallenge/MainApplication.java
+++ android/app/src/main/java/com/bindochallenge/MainApplication.java
@@ -3,6 +3,7 @@ package com.bindochallenge;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.eguma.barcodescanner.BarcodeScannerPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BarcodeScannerPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage()
       );
